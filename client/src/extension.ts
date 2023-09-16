7fda3186be0fe529b8c928604b5aaa8999997852
Laurence Warne <laurencewarne@gmail.com>
2023-09-16 15:08:33 +0100

    Reformat stuff

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -2,61 +2,46 @@ import * as path from 'path';
 import { workspace, ExtensionContext } from 'vscode';
 
 import {
-	LanguageClient,
-	LanguageClientOptions,
-	ServerOptions,
-	TransportKind
+    LanguageClient,
+    LanguageClientOptions,
+    ServerOptions,
+    TransportKind
 } from 'vscode-languageclient/node';
 
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
 
-	// The server is implemented in node
-	const serverModule = context.asAbsolutePath(
-		path.join('server', 'out', 'server.js')
-	);
+    // Options to control the language client
+    const clientOptions: LanguageClientOptions = {
+	// Register the server for plain text documents
+	documentSelector: [{ scheme: 'file', language: 'yaml' }],
+	synchronize: {
+	    // Notify the server about file changes to '.clientrc files contained in the workspace
+	    fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+	}
+    };
 
-	// If the extension is launched in debug mode then the debug server options are used
-	// Otherwise the run options are used
-	const serverOptions: ServerOptions = {
-		run: { module: serverModule, transport: TransportKind.ipc },
-		debug: {
-			module: serverModule,
-			transport: TransportKind.ipc,
-		}
-	};
+    // Create the language client and start the client.
+    client = new LanguageClient(
+	'cfn-lsp-extra',
+	'cfn-lsp-extra Language Server',
+	{ command: "cfn-lsp-extra", args: ["-vv"] },
+	clientOptions
+    );
 
-	// Options to control the language client
-	const clientOptions: LanguageClientOptions = {
-		// Register the server for plain text documents
-		documentSelector: [{ scheme: 'file', language: 'yaml' }],
-		synchronize: {
-			// Notify the server about file changes to '.clientrc files contained in the workspace
-			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
-		}
-	};
-
-	// Create the language client and start the client.
-	client = new LanguageClient(
-       'cfn-lsp-extra',
-       'cfn-lsp-extra Language Server',
-       { command: "cfn-lsp-extra", args: ["-vv"] },
-       clientOptions
-       );
-
-	// Start the client. This will also launch the server
-	client.start();
+    // Start the client. This will also launch the server
+    client.start();
 }
 
 export function deactivate(): Thenable<void> | undefined {
-	if (!client) {
-		return undefined;
-	}
-	return client.stop();
+    if (!client) {
+	return undefined;
+    }
+    return client.stop();
 }
 
 function isCloudFormationTemplate(yamlContent: string): boolean {
-  const cfPattern = /AWSTemplateFormatVersion|Resources|Outputs|Parameters|Mappings/;
+    const cfPattern = /AWSTemplateFormatVersion|/;
     return cfPattern.test(yamlContent);
 }
